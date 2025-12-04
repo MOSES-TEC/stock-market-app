a5c5b8d7fdb256b843d1a27508bad3730e866014
MOSES Y <ymoses546@gmail.com>
2025-12-04 17:07:45 +0530

    ignore eslint and typescript

--- components/TradingViewWidget.tsx
+++ components/TradingViewWidget.tsx
@@ -1,24 +1,31 @@
-"use client";
+'use client';
 
-import React, {useEffect, useRef} from "react";
+import React, { memo } from 'react';
+import useTradingViewWidget from "@/hooks/useTradingViewWidget";
+import { cn } from "@/lib/utils";
 
+interface TradingViewWidgetProps {
+    title?: string;
+    scriptUrl: string;
+    config: Record<string, unknown>;
+    height?: number;
+    className?: string;
+}
 
-const TradingViewWidget = () => {
-    const container = useRef(null);
-
-    useEffect(() => {
-        const script = document.createElement("script");
-        script.src = "";
-        script.type = "text/javascript";
-        script.async = true;
-        script.innerHTML = ``;
-        container.current.appendChild(script);
-    }, []);
+const TradingViewWidget = ({ title, scriptUrl, config, height = 600, className }: TradingViewWidgetProps) => {
+    const containerRef = useTradingViewWidget(scriptUrl, config, height);
 
     return (
-        <div></div>
+        <div className="w-full">
+            {title && <h3 className="font-semibold text-2xl text-gray-100 mb-5">{title}</h3>}
+            <div className={cn('tradingview-widget-container', className)} ref={containerRef}>
+                <div className="tradingview-widget-container__widget" style={{ height, width: "100%" }} />
+            </div>
+        </div>
     );
-};
+}
+
+export default memo(TradingViewWidget);
 
 
 
